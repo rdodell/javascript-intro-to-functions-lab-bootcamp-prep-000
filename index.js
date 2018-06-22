9075bf4a443332f17a4cf59f95879d04d7deb52c
Rachel Dodell <rachel@racheldodell.com>
2018-06-22 21:23:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logShout(string){
 var lowercase = 'hello'
 
 function sayHiToGrandma(string){
-  if lowercase.toLowerCase() === lowercase
-  return "I can't hear you "
+  if (lowercase.toLowerCase()) === lowercase
+  return "I can't hear you"
 }
